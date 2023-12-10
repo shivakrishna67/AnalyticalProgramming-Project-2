85d085721586b72a7ec7b2a45f12e1454f5a57bd
shivakrishna67 <125286143+shivakrishna67@users.noreply.github.com>
2023-12-10 12:54:43 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='my_package',
@@ -6,9 +6,9 @@ setup(
     author='Shivakrishna Macha',
     author_email='smacha@mail.yu.edu',
     description="A package for extracting and analyzing web data",
-    long_description_content_type="text/markdown", # Fixed attribute name and added a comma
+    long_description_content_type="text/markdown",
     url='https://github.com/shivakrishna67/AnalyticalProgramming-Project-2',
-    packages=['my_package'],
+    packages=find_packages(),
     install_requires=[
         "requests>=2.25.1",
         "pandas>=1.2.0",
