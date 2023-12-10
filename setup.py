d9ba8a8216eef0d34d5a2ca779c95fb942a70257
shivakrishna67 <125286143+shivakrishna67@users.noreply.github.com>
2023-12-10 12:32:47 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,9 +6,9 @@ setup(
     author='Shivakrishna Macha',
     author_email='smacha@mail.yu.edu',
     description="A package for extracting and analyzing web data",
-    long_description_content_type="text/markdown",  # Fixed attribute name and added a comma
+    long_description_content_type="text/markdown", # Fixed attribute name and added a comma
     url='https://github.com/shivakrishna67/AnalyticalProgramming-Project-2',
-    packages=['Motor_vehicle_collisions'],
+    packages=['my_package'],
     install_requires=[
         "requests>=2.25.1",
         "pandas>=1.2.0",
